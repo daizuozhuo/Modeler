b4560054d4cb4e6dfa96983aa806c74013c91822
Dai Zuozhuo <daizuozhuo@gmail.com>
2014-03-12 21:47:11 +0800

    add leg

--- gundan.cpp
+++ gundan.cpp
@@ -14,6 +14,9 @@ public:
     Gundan(int x, int y, int w, int h, char *label) 
         : ModelerView(x,y,w,h,label) { }
     virtual void draw();
+private:
+	void drawRightleg();
+	void drawLeftleg();
 };
 
 
@@ -29,6 +32,19 @@ void drawQuad(v3 a, v3 b, v3 c, v3 d) {
 	drawTriangle(a, b, c);
 	drawTriangle(c, d, a);
 }
+
+void drawQuadruple(v3 a1, v3 b1, v3 c1, v3 d1,
+				   v3 a2, v3 b2, v3 c2, v3 d2)
+{
+	//a1, b1, c1, d1 are belong to the bottom surface
+	drawQuad(d1, c1, b1, a1);
+	drawQuad(a1, a2, d2, d1);
+	drawQuad(d1, d2, c2, c1);
+	drawQuad(c1, c2, b2, b1);
+	drawQuad(b1, b2, a2, a1);
+	drawQuad(a2, b2, c2, d2);
+}
+
 
 void drawPrism(double x1, double y1, double z1,
 			   double x2, double y2, double z2,
@@ -60,6 +76,135 @@ ModelerView* createGundan(int x, int y, int w, int h, char *label)
     return new Gundan(x,y,w,h,label); 
 }
 
+void Gundan::drawRightleg()
+{
+	//draw the right leg
+	glPushMatrix();
+	//draw upper joint
+	glTranslated(0.1, -1.4, 0);
+	glPushMatrix();
+	{
+		glRotated(90, 0.0, 1.0, 0.0);
+		setDiffuseColor(0.6f, 0.6f, 0.6f);
+		drawCylinder(1.2, 0.3, 0.3);
+	}
+	glPopMatrix();
+
+	//draw upper leg
+	double ulen=-1.5;
+	{
+		v3 a1(0.9, ulen, -0.3);
+		v3 b1(0.3, ulen, -0.3);
+		v3 c1(0.3, ulen, 0.3);
+		v3 d1(0.9, ulen, 0.3);
+		v3 a2(1.0, 0.0, -0.3);
+		v3 b2(0.2, 0.0, -0.3);
+		v3 c2(0.2, 0.0, 0.3);
+		v3 d2(1.0, 0.0, 0.3);
+		setDiffuseColor(1.0f, 1.0f, 1.0f);
+		drawQuadruple(a1, b1, c1, d1, a2, b2, c2, d2);
+	}
+
+	//draw middle joint
+	ulen += -0.1;
+	glTranslated(0.3, ulen, 0);
+	glPushMatrix();
+	{
+		glRotated(90, 0.0, 1.0, 0.0);
+		setDiffuseColor(0.6f, 0.6f, 0.6f);
+		drawCylinder(0.6, 0.3, 0.3);
+	}
+	glPopMatrix();
+
+	//draw lower leg
+	double llen=-2.0;
+	glTranslated(0.0, llen, -0.25);
+	setDiffuseColor(1.0f, 1.0f, 1.0f);
+	drawBox(0.6, abs(llen), 0.5);
+
+	//draw foot
+	double footheight=0.5;
+	glTranslated(-0.2, -footheight, 0);
+	v3 a1(0.9, 0, -0.1);
+	v3 b1(0, 0, -0.1);
+	v3 c1(0.2, 0, 1.2);
+	v3 d1(0.7, 0, 1.2);
+	v3 a2(0.8, footheight, 0);
+	v3 b2(0.2, footheight, 0);
+	v3 c2(0.2, footheight, 0.6);
+	v3 d2(0.8, footheight, 0.6);
+	setDiffuseColor(COLOR_RED);
+	drawQuadruple(a1, b1, c1, d1, a2, b2, c2, d2);
+
+	glPopMatrix();
+}
+
+void Gundan::drawLeftleg()
+{
+	glPushMatrix();
+	//draw upper joint
+	glTranslated(-0.1, -1.4, 0);
+	glPushMatrix();
+	{
+		glRotated(-90, 0.0, 1.0, 0.0);
+		setDiffuseColor(0.6f, 0.6f, 0.6f);
+		drawCylinder(1.2, 0.3, 0.3);
+	}
+	glPopMatrix();
+
+	//draw upper leg
+	double ulen=-1.5;
+	{
+		v3 a1(-0.9, ulen, -0.3);
+		v3 b1(-0.3, ulen, -0.3);
+		v3 c1(-0.3, ulen, 0.3);
+		v3 d1(-0.9, ulen, 0.3);
+		v3 a2(-1.0, 0.0, -0.3);
+		v3 b2(-0.2, 0.0, -0.3);
+		v3 c2(-0.2, 0.0, 0.3);
+		v3 d2(-1.0, 0.0, 0.3);
+		setDiffuseColor(1.0f, 1.0f, 1.0f);
+		drawQuadruple(d1, c1, b1, a1, d2, c2, b2, a2);
+	}
+
+	//draw middle joint
+	ulen += -0.1;
+	glTranslated(-0.3, ulen, 0);
+	glPushMatrix();
+	{
+		glRotated(-90, 0.0, 1.0, 0.0);
+		setDiffuseColor(0.6f, 0.6f, 0.6f);
+		drawCylinder(0.6, 0.3, 0.3);
+	}
+	glPopMatrix();
+
+	//draw lower leg
+	double llen=-2.0;
+	glTranslated(0.0, llen, -0.25);
+	glPushMatrix();
+	glTranslated(-0.6, 0, 0);
+	setDiffuseColor(1.0f, 1.0f, 1.0f);
+	drawBox(0.6, abs(llen), 0.5);
+	glPopMatrix();
+
+	//draw foot
+	double footheight=0.5;
+	glTranslated(0.2, -footheight, 0);
+	v3 a1(-0.9, 0, -0.1);
+	v3 b1(-0, 0, -0.1);
+	v3 c1(-0.2, 0, 1.2);
+	v3 d1(-0.7, 0, 1.2);
+	v3 a2(-0.8, footheight, 0);
+	v3 b2(-0.2, footheight, 0);
+	v3 c2(-0.2, footheight, 0.6);
+	v3 d2(-0.8, footheight, 0.6);
+	setDiffuseColor(COLOR_RED);
+	drawQuadruple(d1, c1, b1, a1, d2, c2, b2, a2);
+
+	glPopMatrix();
+}
+
+
 // We are going to override (is that the right word?) the draw()
 // method of ModelerView to draw out Gundan
 void Gundan::draw()
@@ -68,6 +213,7 @@ void Gundan::draw()
 	setAmbientColor(.1f,.1f,.1f);
 	//draw the head
 	glPushMatrix();
+	{
 		glTranslated(-0.5, 2, -0.5);
 		setDiffuseColor(1.0f, 1.0f, 1.0f);
 		drawBox(1, 1, 1);
@@ -77,13 +223,16 @@ void Gundan::draw()
 		setDiffuseColor(1.0f, 1.0f, 0);
 		drawPrism(0, 0, 0, 0, 0.4, 0, -1.6, 1.6, 0, 0.1);
 		drawPrism(0.4, 0, 0, 2.0, 1.6, 0, 0.4, 0.4, 0, 0.1);
+	}
 	glPopMatrix();
 
 	//draw the body
 	glPushMatrix();
+	{
 		glTranslated(-1, -1, -0.5);
 		setDiffuseColor(COLOR_BLUE);
 		drawBox(2, 2.5, 1);
+	}
 	glPopMatrix();
 
 	double armLength = 4.0;
@@ -130,16 +279,9 @@ void Gundan::draw()
 		drawPrism(a, b, c, 1.0);
 	}
 	glPopMatrix();
-
-	//draw the right leg
-	glPushMatrix();
-	{
-		glTranslated(0.1, -1.4, 0);
-		glRotated(90, 0.0, 1.0, 0.0);
-		setDiffuseColor(0.6f, 0.6f, 0.6f);
-		drawCylinder(1.2, 0.3, 0.3);
-	}
-	glPopMatrix();
+	drawRightleg();
+	drawLeftleg();
+	
 }
 
 int main()
